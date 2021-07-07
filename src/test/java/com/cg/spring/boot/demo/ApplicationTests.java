fd6ae99103ce098b76346ca1fc1e7beea059d79c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-07-07 15:23:38 +0530

    Update ApplicationTests.java

--- src/test/java/com/cg/spring/boot/demo/ApplicationTests.java
+++ src/test/java/com/cg/spring/boot/demo/ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.cg.spring.boot.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,17 +13,24 @@ class ApplicationTests {
 
 	private static Logger LOG = LoggerFactory.getLogger(ApplicationTests.class);
 
+	// positive test case
 	@Test
 	public void testHello() {
-
 		LOG.info("testHello");
-
 		Hello obj = new Hello();
-
 		String expected = "Hello world!";
 		String actual = obj.hello();
-
 		assertEquals(expected, actual);
-
 	}
+
+	// negative test case
+	@Test
+	public void testHello2() {
+		LOG.info("testHello");
+		Hello obj = new Hello();
+		String unexpected = "abcd";
+		String actual = obj.hello();
+		assertNotEquals(unexpected, actual);
+	}
+
 }
